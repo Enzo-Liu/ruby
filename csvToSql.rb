88878106f42de986434de4cc9bd42e72d234cb8a
liuenze <liu.enze@me.com>
2014-01-26 16:11:31 +0800

    filter \r\n to \n

--- csvToSql.rb
+++ csvToSql.rb
@@ -1,11 +1,10 @@
 #encoding=UTF-8
 arg=ARGV[0]
-open("#{arg}") do |file|  
-  file.each{|line| 
+text = File.open("#{arg}").read 
+text.gsub!(/\r\n?/,"\n")
+text.each_line do |line|
     s = line.to_s
     ss = s.split(",")
-    puts "insert into Temp_K2_Comment1 (ProcInstID,Action,LoginID,RealName,ActivityName,AddDate,ProcessCode) values (\"#{ss[0].chomp()}\",\"#{ss[1]}\",\"#{ss[2]}\",\"#{ss[3]}\",\"#{ss[4]}\",\"#{ss[5]}\",\"#{ss[6].chomp()}\"\n);"
-  }
-
-  file.close();  
-end  
+    #puts "insert into Temp_K2_Comment1 (ProcInstID,Action,LoginID,RealName,ActivityName,AddDate,ProcessCode) values (\"#{ss[0].chomp()}\",\"#{ss[1]}\",\"#{ss[2]}\",\"#{ss[3]}\",\"#{ss[4]}\",\"#{ss[5]}\",\"#{ss[6].chomp()}\"\n);"
+    puts "update `TGP_DEAL_GROUP` set `SUBMIT_FOR_MERCHANT_CONFIRM_DATE`='#{ss[2].chomp}' where `DEAL_GROUP_ID`=#{ss[0]};"
+end
