910e525e82d7ff99373304c67333bfdf07ce6083
liuenze <liu.enze@me.com>
2014-02-09 14:49:05 +0800

    try rubygame

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int f(int x)
+{
+    return x * x;
+}
+ 
+int g(int (*func)(int), int x)
+{
+    return (*func)(x + 1) + (*func)(x - 1);
+}
+ 
+int main()
+{
+    printf("%d\n", g(f, 5));
+    if(1)
+      printf("test\n");
+
+    return 0;
+}
+
