16d225e408565012f56411fd6a50d7e68c2b63a2
liuenze <liu.enze@me.com>
2014-10-14 19:48:32 +0800

    .

--- fib.rb
+++ fib.rb
@@ -1,29 +1,29 @@
-require 'benchmark'  
-include Benchmark  
+require 'benchmark'
+include Benchmark
 
-def fib_iter(n, a = 1, b = 0)  
-  if n == 0 
-    a  
-  else   
-    fib_iter(n - 1, a + b, a)  
-  end  
-end  
-
-def fib(n)  
+def fib_iter(n, a = 1, b = 0)
   if n == 0
-     1  
+    a
+  else
+    fib_iter(n - 1, a + b, a)
+  end
+end
+
+def fib(n)
+  if n == 0
+     1
   elsif n == 1
-     1  
-  else  
-    fib(n-2) + fib(n-1)  
-  end  
-end  
-# bmbm(12) do |x|  
-#   x.report("fib") { 1000.times { fib(30) } }  
-#   x.report("fib_iter") { 1000.times { fib_iter(30) } }  
-# end  
+     1
+  else
+    fib(n-2) + fib(n-1)
+  end
+end
+# bmbm(12) do |x|
+#   x.report("fib") { 1000.times { fib(30) } }
+#   x.report("fib_iter") { 1000.times { fib_iter(30) } }
+# end
 def foo (n)
-  lambda {|i| n += i } 
+  lambda {|i| n += i }
 end
 
 puts "Heeeeey"=~/e+y/
